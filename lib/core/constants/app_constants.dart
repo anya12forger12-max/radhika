85084113ab4cd57dcfa4947dfcda32e35806ba2e
anya <anya@anya.bbrouter>
2026-09-25 14:19:44 +0530

    chore: bump version to 1.2.23+28

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   AppConstants._();
 
   static const String appName = 'Radhika';
-  static const String appVersion = '1.2.22';
+  static const String appVersion = '1.2.23';
   static const String privacyPolicyUrl =
       'https://raw.githubusercontent.com/anya12forger12-max/radhika/main/PRIVACY.md';
   static const String privacyPolicyVersion = '1.1.0';
